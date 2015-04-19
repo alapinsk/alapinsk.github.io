5dc5912f4d1f0bebacd404bef86ed4a905ec50da
alapinsk <a.lapinski@outlook.com>
2015-04-19 09:21:48 +0200

    Update 2015-04-19T07:21:44.540Z

--- scripts/animation.js
+++ scripts/animation.js
@@ -23,6 +23,29 @@ var isMobile = {
     }
 };
 
+
+//loading page 
+function onReady(callback) {
+    var intervalID = window.setInterval(checkReady, 1000);
+
+    function checkReady() {
+        if (document.getElementsByTagName('canvas')[0] !== undefined) {
+            window.clearInterval(intervalID);
+            callback.call(this);
+        }
+    }
+}
+
+function show(id, value) {
+    document.getElementById(id).style.display = value ? 'block' : 'none';
+}
+
+onReady(function () {
+    show('page', true);
+    show('loading', false);
+});
+
+
 /**
  * Makes a nice constellation on canvas
  * @constructor Constellation
