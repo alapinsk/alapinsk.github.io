5c606cb253e034a567ba28e53f942e38155f352c
alapinsk <a.lapinski@outlook.com>
2015-04-19 10:09:13 +0200

    Update 2015-04-19T08:09:09.357Z

--- scripts/app.js
+++ scripts/app.js
@@ -24,6 +24,24 @@ alapinskApp.controller('ModalDemoCtrl', function ($scope, $modal, $log) {
       $log.info('Modal dismissed at: ' + new Date());
     });
   };
+
+
+});
+
+angular.module('alapinskApp').controller('CarouselDemoCtrl', function ($scope) {
+  $scope.myInterval = 5000;
+  var slides = $scope.slides = [];
+  $scope.addSlide = function() {
+    var newWidth = 600 + slides.length + 1;
+    slides.push({
+      image: 'http://placekitten.com/' + newWidth + '/300',
+      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+    });
+  };
+  for (var i=0; i<4; i++) {
+    $scope.addSlide();
+  }
 });
 
 // Please note that $modalInstance represents a modal window (instance) dependency.
@@ -43,5 +61,6 @@ alapinskApp.controller('ModalInstanceCtrl', function ($scope, $modalInstance, it
   $scope.cancel = function () {
     $modalInstance.dismiss('cancel');
   };
+
 });
 
